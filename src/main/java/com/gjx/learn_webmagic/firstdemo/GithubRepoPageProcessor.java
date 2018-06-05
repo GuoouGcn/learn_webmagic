4b76a0961a2dac2e3de6716ad96ea37d9964eba0
v_guojixian <v_guojixian@baidu.com>
2018-06-05 10:20:47 +0800

    初始提交

---
+++ src/main/java/com/gjx/learn_webmagic/firstdemo/GithubRepoPageProcessor.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
+ */
+package com.gjx.learn_webmagic.firstdemo;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * @author: guojixian
+ * @create: 2018-06-05 10:18
+ **/
+public class GithubRepoPageProcessor implements PageProcessor {
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
