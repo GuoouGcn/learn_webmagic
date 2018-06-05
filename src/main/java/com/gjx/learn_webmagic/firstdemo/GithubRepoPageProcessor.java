4390d6b825f2222e0f302af8f4c44a3d99b53d6d
v_guojixian <v_guojixian@baidu.com>
2018-06-05 16:44:15 +0800

    添加爬虫demo

--- src/main/java/com/gjx/learn_webmagic/firstdemo/GithubRepoPageProcessor.java
+++ src/main/java/com/gjx/learn_webmagic/firstdemo/GithubRepoPageProcessor.java
@@ -5,6 +5,8 @@ package com.gjx.learn_webmagic.firstdemo;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
@@ -12,13 +14,31 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * @create: 2018-06-05 10:18
  **/
 public class GithubRepoPageProcessor implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
     @Override
     public void process(Page page) {
-
+        page.addTargetRequests(page.getHtml().links().regex("(https://www\\.jianshu\\.com/\\w+/\\w+)").all());
+        page.putField("author", page.getUrl().regex("https://www\\.jianshu\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name")==null){
+            //skip this page
+            page.setSkip(true);
+        }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
     }
 
     @Override
     public Site getSite() {
-        return null;
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new GithubRepoPageProcessor())
+                .addUrl("https://www.jianshu.com/p/f8ddef9846d7")
+                .addPipeline(new JsonFilePipeline("classpath：\\result\\"))
+                .thread(5)
+                .run();
     }
 }
