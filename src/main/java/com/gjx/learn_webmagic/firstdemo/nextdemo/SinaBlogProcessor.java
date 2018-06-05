96885069e1f63b5cd75bb5cd27466d33155489da
v_guojixian <v_guojixian@baidu.com>
2018-06-05 19:58:12 +0800

    添加 爬虫demo

--- src/main/java/com/gjx/learn_webmagic/firstdemo/nextdemo/SinaBlogProcessor.java
+++ src/main/java/com/gjx/learn_webmagic/firstdemo/nextdemo/SinaBlogProcessor.java
@@ -18,6 +18,9 @@ public class SinaBlogProcessor implements PageProcessor {
 
     public static final String URL_POST = "http://blog\\.sina\\.com\\.cn/s/blog_\\w+\\.html";
 
+    /**
+     * 抓取网站的相关配置, 包括 编码,抓取间隔,重试次数
+     */
     private Site site = Site
             .me()
             .setDomain("blog.sina.com.cn")
@@ -25,6 +28,10 @@ public class SinaBlogProcessor implements PageProcessor {
             .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko)"
                     + " Chrome/26.0.1410.65 Safari/537.31");
 
+    /**
+     * process 定制爬虫逻辑的核心接口, 编写抽取逻辑
+     * @param page
+     */
     @Override
     public void process(Page page) {
         //列表页
@@ -47,6 +54,7 @@ public class SinaBlogProcessor implements PageProcessor {
     public static void main(String[] args) {
         Spider.create(new SinaBlogProcessor())
                 .addUrl("http://blog.sina.com.cn/s/articlelist_1487828712_0_1.html")
+                // 启动爬虫
                 .run();
     }
 }
